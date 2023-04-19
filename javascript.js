dac7d5954b694456e5e93e86ad8d95895516026f
G-Alessandro <galloalessandro77@gmail.com>
2023-04-19 17:59:17 +0200

    Modified endGame function for better readability

--- javascript.js
+++ javascript.js
@@ -10,45 +10,95 @@ const playerTwo = {
   point: '',
 };
 
-const playerSign = ['', '', '', '', '', '', '', '', ''];
-let scorePlayer1 = 0;
-let scorePlayer2 = 0;
-
-// 0-2    3-5    6-8   0-3-6   1-4-7   2-5-8   0-4-8   2-4-6
 const functionModule = (() => {
+  let scorePlayer1 = 0;
+  let scorePlayer2 = 0;
+  const score1 = document.getElementById('score1');
+  const score2 = document.getElementById('score2');
+  score1.textContent = scorePlayer1;
+  score2.textContent = scorePlayer2;
+  const playerSign = ['', '', '', '', '', '', '', '', ''];
+
   const endGame = () => {
-    for (let i = 0; i < 6; i + 3) {
-      if (playerSign.slice(i, (i + 3)).join('') === 'xxx') {
-        scorePlayer1++;
-      }
-      if (playerSign.slice(i, (i + 3)).join('') === 'ooo') {
-        scorePlayer2++;
-      }
+    // 0-2    3-5    6-8   0-3-6   1-4-7   2-5-8   0-4-8   2-4-6
+    const arrZero = playerSign[0];
+    const arrOne = playerSign[1];
+    const arrTwo = playerSign[2];
+    const arrThree = playerSign[3];
+    const arrFour = playerSign[4];
+    const arrFive = playerSign[5];
+    const arrSix = playerSign[6];
+    const arrSeven = playerSign[7];
+    const arrEight = playerSign[8];
+
+    const vertResOne = arrZero + arrThree + arrSix;
+    const vertResTwo = arrOne + arrFour + arrSeven;
+    const vertResThree = arrTwo + arrFive + arrEight;
+    const diagResOne = arrZero + arrFour + arrEight;
+    const diagResTwo = arrTwo + arrFour + arrSix;
+
+    if (playerSign.slice(0, 3).join('') === 'xxx'
+    || playerSign.slice(3, 6).join('') === 'xxx'
+    || playerSign.slice(6, 9).join('') === 'xxx'
+    || vertResOne === 'xxx'
+    || vertResTwo === 'xxx'
+    || vertResThree === 'xxx'
+    || diagResOne === 'xxx'
+    || diagResTwo === 'xxx') {
+      scorePlayer1++;
     }
-    for (let i = 0; i < 2; i++) {
-      const first = playerSign[i];
-      const second = playerSign[i + 3];
-      const third = playerSign[i + 6];
-      const indexConcat = first + second + third;
-      if (indexConcat === 'xxx') {
-        scorePlayer1++;
-      }
-      if (indexConcat === 'ooo') {
-        scorePlayer2++;
-      }
+    if (playerSign.slice(0, 3).join('') === 'ooo'
+    || playerSign.slice(3, 6).join('') === 'ooo'
+    || playerSign.slice(6, 9).join('') === 'ooo'
+    || vertResOne === 'ooo'
+    || vertResTwo === 'ooo'
+    || vertResThree === 'ooo'
+    || diagResOne === 'ooo'
+    || diagResTwo === 'ooo') {
+      scorePlayer2++;
     }
-    for (let i = 0; i < 2; i + 2) {
-      const first = playerSign[i];
-      const second = playerSign[4];
-      const third = playerSign[8 - i];
-      const indexConcat = first + second + third;
-      if (indexConcat === 'xxx') {
-        scorePlayer1++;
-      }
-      if (indexConcat === 'ooo') {
-        scorePlayer2++;
-      }
-    }
+
+    // for (let a = 0; a < 6; a + 3) {
+    //   const first = playerSign[a];
+    //   const second = playerSign[a + 1];
+    //   const third = playerSign[a + 2];
+    //   const indexConcat = first + second + third;
+    //   if (playerSign.slice(a, (a + 3)).join('') === 'xxx') {
+    //     scorePlayer1++;
+    //   }
+    //   if (playerSign.slice(a, (a + 3)).join('') === 'ooo') {
+    //     scorePlayer2++;
+    //   }
+    //   if (indexConcat !== 'xxx' || indexConcat !== 'ooo') { break; }
+    // }
+    // for (let e = 0; e < 2; e++) {
+    //   const first = playerSign[e];
+    //   const second = playerSign[e + 3];
+    //   const third = playerSign[e + 6];
+    //   const indexConcat = first + second + third;
+    //   if (indexConcat === 'xxx') {
+    //     scorePlayer1++;
+    //   }
+    //   if (indexConcat === 'ooo') {
+    //     scorePlayer2++;
+    //   }
+    // }
+    // for (let d = 0; d < 2; d + 2) {
+    //   const first = playerSign[d];
+    //   const second = playerSign[4];
+    //   const third = playerSign[8 - d];
+    //   const indexConcat = first + second + third;
+    //   if (indexConcat === 'xxx') {
+    //     scorePlayer1++;
+    //   }
+    //   if (indexConcat === 'ooo') {
+    //     scorePlayer2++;
+    //   }
+    // }
+    score1.textContent = scorePlayer1;
+    score2.textContent = scorePlayer2;
+    console.log(scorePlayer1);
+    console.log(scorePlayer2);
   };
 
   const signTotal = () => {
@@ -85,10 +135,11 @@ const functionModule = (() => {
           squareDiv.textContent = 'X';
           playerSign.splice(i, 1, 'x');
         }
+        endGame();
       });
     }
   };
-  return { signTotal, assignSign };
+  return { endGame, signTotal, assignSign };
 })();
 
 functionModule.assignSign();
